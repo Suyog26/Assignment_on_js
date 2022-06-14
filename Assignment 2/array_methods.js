44b5778ffc66a0364802c047e20fc4e26ac41d56
Suyog26 <rautsuyog4@gamil.com>
2022-06-14 18:51:49 +0530

    second assignment added with array methods

---
+++ Assignment 2/array_methods.js
@@ -0,0 +1,76 @@
+let myarray =[1,2,3,4,"rishab","shreyas","ishan"];
+
+let position=[0,0,0,0,"bat","bat","bat"]
+
+// // to add any value to array 
+// myarray[7]="virat";
+// console.log(myarray);
+
+// // to replace any value from the array 
+// myarray[3]="ms dhoni"
+// console.log(myarray);
+
+// // total no items present in the array
+
+// console.log(myarray.length);
+// // to view from the index value 
+// // console.log(myarray[4]);
+
+// CONCAT METHOD(
+// console.log(position.concat(myarray));
+
+// // POP METHOD = remove the last element 
+// console.log(myarray.pop());
+// console.log(position.pop(4));
+
+// push method = add the element and return the length of the array 
+// console.log(myarray.push("umran"));
+
+// // REMOVE METHOD 
+// console.log(myarray.reverse());
+
+// SHIFT METHOD TO REMOVE THE FIRST ELEMENST OF AN ARRAY
+// console.log(myarray.shift());
+
+// SORT METHOD TO SORT THEM IN ALPHA ORDER OR NUMBER 
+// console.log(myarray.sort());
+// console.log(position.sort());
+
+// TOSTRING =TO GET IN THE STRING FORMAT
+// console.log(myarray.toString());
+
+// unshift=to add element in starting of an array 
+// console.log(myarray.unshift("watches","glasses"));
+// console.log(myarray);
+
+
+// array.prototype.at()
+// let index = 4;
+
+// console.log(` ${index} the item returned is ${myarray.at(index)}`);
+
+// index = 6;
+
+// console.log(` ${index} item returned is ${myarray.at(index)}`);
+
+// copywithin()= The copyWithin() method shallow copies part of an array to another location in the same array and returns it without modifying its length.
+
+// console.log(position.copyWithin(0, 3, 4));
+
+// console.log(position.copyWithin(1, 3));
+
+// includes methods
+// console.log(myarray.includes("kumar"));
+// console.log(myarray.includes("rishab"));
+
+// indexof methods
+// console.log(myarray.indexOf("rishab"));
+// console.log(myarray.indexOf("shreyas"));
+
+// fill
+// console.log(position.fill(0,2,6));
+
+// flat= creates a sub array 
+// console.log(myarray.flat(4));
+
+
---
+++ Assignment 2/assign_2.js
@@ -0,0 +1,312 @@
+const details={
+    "status": "OK",
+    "code": 200,
+    "total": 10,
+    "data" : [
+      {
+        "id": 1,
+        "firstname": "Olivie",
+        "lastname": "Schmitt",
+        "email": "laetitia.blanc@bouygtel.fr",
+        "phone": "+1087245119352",
+        "birthday": "2008-08-10",
+        "gender": "female",
+        "address": {
+          "id": 0,
+          "street": "41, chemin de Hamon",
+          "streetName": "rue Guilbert",
+          "buildingNumber": "3",
+          "city": "Duhamel",
+          "zipcode": "21174",
+          "country": "Afghanistan",
+          "county_code": "YT",
+          "latitude": 32.413095,
+          "longitude": 5.171384
+        },
+        "website": "http://gilles.org",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 2,
+        "firstname": "Luc",
+        "lastname": "Guillot",
+        "email": "jlacroix@labbe.fr",
+        "phone": "+2666204956507",
+        "birthday": "1997-04-27",
+        "gender": "male",
+        "address": {
+          "id": 0,
+          "street": "9, avenue Guy Moulin",
+          "streetName": "rue Benoit",
+          "buildingNumber": "4",
+          "city": "Guilletboeuf",
+          "zipcode": "36513",
+          "country": "Grenade",
+          "county_code": "CF",
+          "latitude": -45.234662,
+          "longitude": 43.927153
+        },
+        "website": "http://pruvost.net",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 3,
+        "firstname": "Adèle",
+        "lastname": "Lefort",
+        "email": "regnier.lucas@wanadoo.fr",
+        "phone": "+6193939060726",
+        "birthday": "1990-02-01",
+        "gender": "female",
+        "address": {
+          "id": 0,
+          "street": "89, chemin de Ledoux",
+          "streetName": "boulevard de Lemaitre",
+          "buildingNumber": "830",
+          "city": "Simondan",
+          "zipcode": "65 833",
+          "country": "Suriname",
+          "county_code": "LB",
+          "latitude": -79.144712,
+          "longitude": 156.124275
+        },
+        "website": "http://perez.com",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 4,
+        "firstname": "Suzanne",
+        "lastname": "Rocher",
+        "email": "claudine.lacombe@tiscali.fr",
+        "phone": "+7901550473323",
+        "birthday": "1953-11-15",
+        "gender": "female",
+        "address": {
+          "id": 0,
+          "street": "98, avenue Thierry",
+          "streetName": "place Lucas Adam",
+          "buildingNumber": "70",
+          "city": "Lemoine",
+          "zipcode": "28 831",
+          "country": "Ukraine",
+          "county_code": "JP",
+          "latitude": -70.403211,
+          "longitude": 175.104862
+        },
+        "website": "http://mercier.fr",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 5,
+        "firstname": "Rémy",
+        "lastname": "Fleury",
+        "email": "etienne.hamon@pruvost.fr",
+        "phone": "+7321790280900",
+        "birthday": "2015-02-04",
+        "gender": "male",
+        "address": {
+          "id": 0,
+          "street": "2, rue Valentine Monnier",
+          "streetName": "place Margaux Guillet",
+          "buildingNumber": "67",
+          "city": "Raymond",
+          "zipcode": "16648",
+          "country": "Dominique",
+          "county_code": "US",
+          "latitude": -24.288796,
+          "longitude": 102.540318
+        },
+        "website": "http://bazin.com",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 6,
+        "firstname": "Guy",
+        "lastname": "Poirier",
+        "email": "gillet.xavier@perret.fr",
+        "phone": "+9287870136197",
+        "birthday": "1969-08-28",
+        "gender": "male",
+        "address": {
+          "id": 0,
+          "street": "4, place de Guillaume",
+          "streetName": "impasse de Rousset",
+          "buildingNumber": "36",
+          "city": "De Oliveira",
+          "zipcode": "64 917",
+          "country": "Yougoslavie",
+          "county_code": "LU",
+          "latitude": 16.886041,
+          "longitude": -98.862444
+        },
+        "website": "http://laroche.net",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 7,
+        "firstname": "Gabriel",
+        "lastname": "Lagarde",
+        "email": "berger.theophile@club-internet.fr",
+        "phone": "+8016728691131",
+        "birthday": "1972-12-16",
+        "gender": "male",
+        "address": {
+          "id": 0,
+          "street": "91, impasse de Chevallier",
+          "streetName": "boulevard de Thomas",
+          "buildingNumber": "402",
+          "city": "Lebretondan",
+          "zipcode": "69079",
+          "country": "Mariannes du Nord (Îles)",
+          "county_code": "GH",
+          "latitude": 67.639497,
+          "longitude": 130.322616
+        },
+        "website": "http://rodriguez.fr",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 8,
+        "firstname": "Margot",
+        "lastname": "Thierry",
+        "email": "claude.imbert@weiss.fr",
+        "phone": "+2723473727963",
+        "birthday": "1941-12-04",
+        "gender": "female",
+        "address": {
+          "id": 0,
+          "street": "211, rue de Brunet",
+          "streetName": "boulevard Michel",
+          "buildingNumber": "897",
+          "city": "Chartier-les-Bains",
+          "zipcode": "34982",
+          "country": "Inde",
+          "county_code": "MA",
+          "latitude": 81.160138,
+          "longitude": 179.895105
+        },
+        "website": "http://moulin.fr",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 9,
+        "firstname": "Alfred",
+        "lastname": "Robert",
+        "email": "jules.pasquier@guillon.org",
+        "phone": "+8168420686847",
+        "birthday": "1935-09-01",
+        "gender": "male",
+        "address": {
+          "id": 0,
+          "street": "2, place Fontaine",
+          "streetName": "rue Georges Bonnet",
+          "buildingNumber": "95",
+          "city": "Leclerc-la-Forêt",
+          "zipcode": "32 635",
+          "country": "Sahara Occidental",
+          "county_code": "HU",
+          "latitude": 70.417116,
+          "longitude": 161.786613
+        },
+        "website": "http://guilbert.com",
+        "image": "http://placeimg.com/640/480/people"
+      },
+      {
+        "id": 10,
+        "firstname": "Adrienne",
+        "lastname": "Schneider",
+        "email": "sgrondin@yahoo.fr",
+        "phone": "+5500511794753",
+        "birthday": "1975-12-22",
+        "gender": "female",
+        "address": {
+          "id": 0,
+          "street": "7, boulevard Dominique Cousin",
+          "streetName": "avenue Margot Ruiz",
+          "buildingNumber": "53",
+          "city": "Benoit",
+          "zipcode": "35952",
+          "country": "Marshall (Îles)",
+          "county_code": "ER",
+          "latitude": 37.731914,
+          "longitude": 35.024487
+        },
+        "website": "http://dubois.com",
+        "image": "http://placeimg.com/640/480/people"
+      }
+    ]
+  }
+
+
+// console.log(details.data);
+
+// let data = details.data.map((data)=>{
+//     return data
+// });
+
+// console.log(data);
+
+
+//below map function can be used for various data manipulation and to get data 
+// let fname = details.data.map((data)=>{
+//     return data.firstname
+// });
+
+// console.log(fname);
+
+// below function is used to get the mini data with the first name last name email 
+
+// let minidata = details.data.map((data)=>({firstname:data.firstname,lastname:data.lastname,email:data.email}));
+
+// console.log(minidata);
+
+// FILTER ()
+
+// below function is used to filter the data with gender :female 
+
+// const gender = details.data.filter((data)=>{
+//     return data.gender == "female"
+// })
+
+// console.log(gender);
+
+// below function is used to filter data with respect to building no < 50
+// const buildingNumber_ss = details.data.filter((data)=>{
+//     return data.address.buildingNumber < 50 ;
+// })
+
+// console.log(buildingNumber_ss);
+
+//REDUCE 
+
+// GET THE TOTAL ID
+// const totalids = details.data.reduce((acc,curr)=>{
+//     return acc + curr.id ;
+// },0);
+
+// console.log(totalids);
+
+// //GET THE TOTAL NO OF phone no 
+// const total_no = details.data.reduce((acc,curr)=> acc + curr.phone.length,0);
+
+// console.log(total_no);
+
+// SLICE COMMAND 
+//BY JUST USING A SLICE COMMAND WITH EMPTY ARGUMENTS WILL CREATE A COPY OF AN ARRAY BELOW EXAMPLE 
+// const slice_1 =details.data.slice();
+
+// console.log(slice_1);
+
+//  BELOW EXAMPLE IS USED TO SLICE THE ARRAY FROM ID:0 TO ID:4
+// const slice_1 =details.data.slice(0,4);
+
+// console.log(slice_1);
+
+//JOIN COMMAND 
+
+// const join_1=details.data.join(:);
+// console.log(join_1);
+
+
+// to view particular data with indexvalue
+// console.log(details.data[3]);
+
